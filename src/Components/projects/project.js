4479a15664fda8a2abd387cf60dde28dddb73b62
kiralyjanos91 <kiralyjanos91@gmail.com>
2022-09-07 17:16:00 +0200

    updated portfolio site

--- src/Components/projects/project.js
+++ src/Components/projects/project.js
@@ -1,11 +1,43 @@
-import React from "react"
-import { Row, Col, Button } from "react-bootstrap"
+import React , { useState } from "react"
+import { Row , Col } from "react-bootstrap"
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import Carousel from "react-bootstrap/Carousel"
 
-export default function Project({img, imgOrder, title, description}){
+export default function Project({ img1 , img2 , img3 , imgOrder , title , description }){
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
     <Row className="project">
-        <Col md={{span:4, order:imgOrder || "first"}}>
-            <img src={img}  className="img-fluid rounded project-image" alt="dummyproject"/>
+        <Col md={{span:6, order:imgOrder || "first"}} lg="4">
+            
+            <Carousel className="project-carousel">
+                <Carousel.Item className="carousel-project-image" onClick={handleShow}>
+                    <img
+                        className="d-block w-100"
+                        src={img1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item onClick={handleShow}>
+                    <img
+                        className="d-block w-100"
+                        src={img2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item onClick={handleShow}>
+                    <img
+                        className="d-block w-100"
+                        src={img3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
         </Col>
         <Col>
             <Row>
@@ -23,6 +55,21 @@ export default function Project({img, imgOrder, title, description}){
                 </Col>
             </Row>
         </Col>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{description}</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Visit website
+                </Button>
+            </Modal.Footer>
+      </Modal>
     </Row> 
     )
 }
